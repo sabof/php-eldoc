6fb2b7c582235ba01ceca5ea7f5631368cd53100
sabof <sabof@example.com>
2013-01-31 14:29:07 +0000

    + header doc

--- php-eldoc.el
+++ php-eldoc.el
@@ -4,7 +4,8 @@
 ;;; URL: https://github.com/sabof/php-eldoc
 
 ;;; Commentary:
-
+;; You should find probe.php in the same directory as this file.
+;;
 ;; The project is hosted at https://github.com/sabof/php-eldoc
 ;; The latest version, and all the relevant information can be found there.
 
